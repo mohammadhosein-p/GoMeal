acc198f9d11a5800f99fea06278f2fa5648deea9
mohammadhosein-p <150898771+mohammadhosein-p@users.noreply.github.com>
2024-12-20 12:10:02 +0330

    added login / signup / logout functions & components and updated mutations to use token for requests

--- src/components/sideBar/card/CheckoutModal.jsx
+++ src/components/sideBar/card/CheckoutModal.jsx
@@ -53,7 +53,7 @@ function CheckoutModal({ isOpen, onClose, className, total }) {
   });
 
   useEffect(() => {
-    if (isOpen && card.length === 0 ) {
+    if (isOpen && card.length === 0) {
       setMessage({
         isActive: true,
         text: "Card is empty!",
